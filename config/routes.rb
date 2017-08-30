85114358ce99522b90ec5bf8e83a365c805b6e81
AubreyBellis <contactaubreybellis@gmail.com>
2017-08-30 14:11:25 -0400

    user setup with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
- root to: 'home#home'
- resources :cities
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root to: 'home#home'
   resources :cities
+  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
