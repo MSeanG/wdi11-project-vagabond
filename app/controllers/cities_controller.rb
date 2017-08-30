a669f114edfad02ef0917330c22afd2abcf0584b
AubreyBellis <contactaubreybellis@gmail.com>
2017-08-30 11:19:31 -0400

    cities_controller

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,4 +1,49 @@
 class CitiesController < ApplicationController
   def index 
+    @cities = City.all
+  end
+#new 
+def new
+    @city = City.new
+  end
+
+  # create
+  def create
+    @city = City.create!(city_params)
+
+    
+    redirect_to cities_path(@city)
+  end
+
+  #show
+  def show
+    @city = City.find(params[:id])
+  end
+
+  # edit
+  def edit
+    @city = City.find(params[:id])
+  end
+
+
+  # update 
+  def update
+    @city = City.find(params[:id])
+    @city.update(city_params)
+
+    redirect_to cities_path(@city)
+  end
+
+  # destroy
+  def destroy
+    @city = City.find(params[:id])
+    @city.destroy
+
+    redirect_to cities_path
+  end
+
+  private
+  def city_params
+    params.require(:city).permit(:name, :location)
   end
 end
