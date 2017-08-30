7e39bbd501bdc383a35de3a1da73d6dbeea53f34
whuizenga <will.huizenga@gmail.com>
2017-08-30 15:27:26 -0400

    Added authorizations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  include CanCan::ControllerAdditions
+
   protect_from_forgery with: :exception
   # def home
   #   render html: "Welcome to Vagabond!"
