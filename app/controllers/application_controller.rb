42bbb53f1aa715f6e575121de797141721befdbc
AubreyBellis <contactaubreybellis@gmail.com>
2017-08-30 10:54:50 -0400

    homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # def home
+  #   render html: "Welcome to Vagabond!"
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
- root to: 'cities#index'
+ root to: 'home#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
