5ceb5b85bd2a771ce3af19d5a6da3292d6ca5a9b
AubreyBellis <contactaubreybellis@gmail.com>
2017-08-31 15:57:46 -0400

    updating user page and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ end
 
 
   get "/users/:id", to: "users#show", as: "user_profile"
-
+  get "/users/:id/edit", to: "users#edit", as: "edit_user_profile"
+  put "/users/:id", to: "users#update", as: "update_user_profile"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
