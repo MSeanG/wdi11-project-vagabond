8eefe14b17248ab64e945cf02d9462976ea4ab37
AubreyBellis <contactaubreybellis@gmail.com>
2017-08-31 11:32:25 -0400

    still working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@
 class UsersController < ApplicationController
     def show
     end
+    def edit
+        @user = User.find(params[:id])
+      end
+    # update
+      def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+    
+        redirect_to users_path(@user)
+      end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830180220) do
+ActiveRecord::Schema.define(version: 20170830201638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,9 +26,9 @@ ActiveRecord::Schema.define(version: 20170830180220) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.bigint "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "city_id"
     t.index ["city_id"], name: "index_posts_on_city_id"
   end
 
@@ -54,4 +54,5 @@ ActiveRecord::Schema.define(version: 20170830180220) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "cities"
 end
