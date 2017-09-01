e029dc3e8d3f2f0a165c5b1b6820784806d20b68
ctt1988 <clarence.tooles@gmail.com>
2017-08-31 20:28:52 -0400

    users edit working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,34 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
 
-    def show
-    @user = current_user
-    end
     def new 
     @user = Current_user.new
     end
-    # edit
-  def edit
-    @user = User.find(params[:id])
+
+    def show
     @user = current_user
-  end
+    end
+
+    # edit
+    def edit
+    @user = current_user
+    end
 
 
-  # update
-  def update
-    @user = User.find(params[:id])
+    # update
+    def update
+    @user = current_user
     @user.update(user_params)
+    end
 
-    redirect_to user_profile_path(@user)
-  end
+    #destroy
+    def destroy
+    @user = User.find(params[:id])
+    @user.destroy
 
-  # destroy
-#   def destroy
-#     @user = User.find(params[:id])
-#     @user.destroy
+    redirect_to user_profile_path
+    end
 
-#     redirect_to user_profile_path
-#   end
   private
   def user_params
     params.require(:user).permit(:username, :first_name, :last_name, :image, :location)
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,10 @@ end
     sessions: 'users/sessions'
   }
 
-
-  get "/users/:id", to: "users#show", as: "user_profile"
   get "/users/:id/edit", to: "users#edit", as: "edit_user_profile"
-  put "/users/:id", to: "users#update", as: "update_user_profile"
+  get "/users/:id", to: "users#show", as: "user_profile"
+  patch "/users/:id", to: "users#update"
+  put "/users/:id", to: "users#update"
+  delete "/users/:id", to: "users#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
